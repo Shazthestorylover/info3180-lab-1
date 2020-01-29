4f22395554432dea513ed055d1d0a76f8f0f01d2
Shazthestorylover <shazzamdaustin@gmail.com>
2020-01-29 11:37:12 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -11,15 +11,17 @@ app = Flask(__name__)
 
 '''
 # Routing for your application.
+# Put your routes below this comment
+'''
 
 @app.route('/')
 def home():
     return 'My home page'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-
-# Put your routes below this comment
-'''
 
 
 @app.errorhandler(404)
